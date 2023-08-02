aa2ee06b55b12e564552e27aea8f5ba19053f728
Drakoxw <dragon12xw@gmail.com>
2023-08-02 09:44:03 -0500

    feat: routes ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
-import { useEffect, useState } from 'react';
-
-import { Result } from '@/core/interfaces';
-import { UserList } from '@/components'
-import { UrlApiRamdom } from '@/core/constants';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Home } from '@/pages';
+import { ROUTES } from '@/core/constants';
 
 function App(): JSX.Element {
-  const [users, setUsers] = useState<Result[]>([]);
-
-  useEffect(() => {
-    fetch(UrlApiRamdom)
-      .then((res) => res.json())
-      .then((data) => setUsers(data.results));
-  }, []);
-
   return (
-    <>
-      <main className="h-screen w-screen overflow-hidden bg-zinc-900 text-white p-8 flex flex-col gap-8">
-          <h1 className="text-4xl text-center font-bold">Hola mundo</h1>
-
-          <div className="w-full">
-            {<UserList users={users} />}
-          </div>
-      </main>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.home} element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Home.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+import { Result } from '@/core/interfaces';
+import { UserList } from '@/components';
+import { UrlApiRamdom } from '@/core/constants';
+
+export default function Main() {
+    const [users, setUsers] = useState<Result[]>([]);
+  
+    useEffect(() => {
+      fetch(UrlApiRamdom)
+        .then((res) => res.json())
+        .then((data) => setUsers(data.results));
+    }, []);
+    return (
+      <>
+        <main className="h-screen w-screen overflow-hidden bg-zinc-900 text-white p-8 flex flex-col gap-8">
+          <h1 className="text-4xl text-center font-bold">Hola mundo</h1>
+  
+          <div className="w-full">{<UserList users={users} />}</div>
+        </main>
+      </>
+    );
+  }
