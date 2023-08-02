4d836e3b31b587ee8d61a7790357266e433158fd
Drakoxw <dragon12xw@gmail.com>
2023-08-02 12:24:11 -0500

    feat: navbar estilos ok

--- src/components/app/Navbar.tsx
+++ src/components/app/Navbar.tsx
@@ -15,41 +15,19 @@ const routes = [
 
 export default function NavBar() {
   return (
-    <nav className="bg-gray-500 border-gray-200 ">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="/" className="flex items-center">
-          <img
-            src="https://app.aveonline.co/app/img_informer/Logo.svg"
-            className="h-8 mr-3"
-            alt="Ave Logo"
-          />
-        </a>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
+    <nav className='bg-gray-500 border-gray-200 '>
+      <div className='max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4'>
+        <NavLink to={ROUTES.home}>
+          <div className='flex items-center'>
+            <img
+              src='https://app.aveonline.co/app/img_informer/Logo.svg'
+              className='h-8 mr-3'
+              alt='Ave Logo'
             />
-          </svg>
-        </button>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
+          </div>
+        </NavLink>
+        <div className=' block w-auto' id='navbar-default'>
+          <ul className='font-medium flex flex-colp-0 border-gray-100 rounded-lg flex-row space-x-8 mt-0 border-0'>
             {routes.map(({ path, title }) => {
               return (
                 <NavLink
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // eslint-disable-next-line no-undef
+    require('flowbite/plugin')
+  ]
+
 }
 
